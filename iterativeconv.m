66cde4c0ff28c122e6337b693bb68bc27363e5fc
Avie Vasantlal <avasantlal@gmail.com>
2025-10-02 02:52:42 -0400

    Params & Display

--- iterativeconv.m
+++ iterativeconv.m
@@ -11,3 +11,19 @@ function [x, iterations] = recip(b, x0, tol)
         x = x_new;
     end
 end
+
+% Params
+b = pi;
+tol = 1e-10;
+
+% Initial guess 1
+x0_1 = 1;
+[x0, it1] = recip(b, x0_1, tol);
+
+% Initial guess 2
+x0_2 = 0.1;
+[x2, it2] = recip(b, x0_2, tol);
+
+% Display
+fprintf('Initial guess x0=1: Approx 1/pi = %.12f, Iterations = %d\n', x1, it1);
+fprintf('Initial guess x0=0.1: Approx 1/pi = %.12f, Iterations = %d\n', x2, it2);
